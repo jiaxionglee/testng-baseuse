b6b006d6c79c2f0d2cfa36a3ce035b33d5eca3f5
lijiaxiong <lijiaxiong@benmu-health.com>
2019-01-02 16:09:12 +0800

    up

---
+++ src/test/java/injection/GuiceExampleModule.java
@@ -0,0 +1,14 @@
+package injection;
+
+import com.google.inject.Module;
+
+/**
+ * Created by jiaxiong
+ * on 2019/1/2
+ */
+public class GuiceExampleModule implements Module {
+
+    @Override
+    public void configure(com.google.inject.Binder binder) {
+    }
+}
